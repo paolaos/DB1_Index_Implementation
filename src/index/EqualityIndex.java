04af38662bf82695f4076dc0b6b754b4cd96b374
Paola Ortega S <paola@saborio.org>
2017-04-01 21:55:21 -0600

    Se arregló un error del gitignore, se implementaron los métodos de la clase EqualityIndex y se agregó la clase de UITest (que en este momento está en pruebas).

--- src/index/EqualityIndex.java
+++ src/index/EqualityIndex.java
@@ -1,24 +1,39 @@
 package index;
 
-import java.util.Hashtable;
-import java.util.List;
+import java.util.*;
 import query.*;
 
 /**
  * Created by Paola Ortega S on 3/16/2017.
  */
 public class EqualityIndex implements Index {
-    Hashtable<String, List<Integer>> table;
-
-
+    Hashtable<String, List<Integer>> table = new Hashtable<>();
 
     @Override
     public List<Integer> getObjects(Query query) {
-        return null;
+        List<Integer> result = new LinkedList<>();
+        switch (query.getQueryType()){
+            case EQUALITY:
+                result = table.get(query.getValue1());
+                break;
+            case INEQUALITY:
+                Hashtable<String, List<Integer>> copy = table;
+                copy.remove(query.getValue1());
+                for(List<Integer> temp: copy.values())
+                    result.addAll(temp);
+
+                break;
+        }
+        return result;
     }
 
     @Override
     public void addObject(Object o, Integer row) {
+        String string = (String) o;
+        if(!table.contains(string))
+            table.put(string, new LinkedList<>(Collections.singleton(row)));
+
+        else table.get(string).add(row);
 
     }
 
---
+++ src/tests/UITest.java
@@ -0,0 +1,13 @@
+package tests;
+
+
+import ui.UI;
+
+/**
+ * Created by Paola Ortega S on 4/1/2017.
+ */
+public class UITest {
+    public static void main(String[] args){
+        UI ui = new UI();
+    }
+}
